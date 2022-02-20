588a3c2edeea34a5fdceee684da0c04f5adba521
serhiintus <sergius.sport@gmail.com>
2022-02-20 16:32:29 +0100

    add comments and remove duplicates in a number

--- project_5.2.py
+++ project_5.2.py
@@ -1,21 +1,20 @@
+#this function counts the number of different digits of the number b contained in the number a
 def counter(a, b):
     a = str(a)
     b = str(b)
-    n = 0
     count = 0
-    ax = ''
+    unduplicated_b = ''
+    unduplicated_a = ''
+    #remove duplicates in b
     for i in b:
-        if i not in ax:
-            ax += i
-    for i in ax:
-        for j in a:
+        if i not in unduplicated_b:
+            unduplicated_b += i
+    #remove duplicates in a
+    for i in a:
+        if i not in unduplicated_a:
+            unduplicated_a += i
+    for i in unduplicated_b:
+        for j in unduplicated_a:
             if i == j:
-                n += 1
-        if n > 0:
-            count += 1
-            n = 0
+                count += 1
     return count
-
-x = 98123560
-y = 79266
-print(counter(x, y))
