5ed1131a0354905057ddfb18e66db83e276432a9
serhiintus <sergius.sport@gmail.com>
2022-02-20 14:47:47 +0100

    add comments and turn the for loop into list comprehension

--- project_5.1.py
+++ project_5.1.py
@@ -1,12 +1,8 @@
+#this function removes duplicates of the same values of the same types
 def clean_list(list_to_clean):
     for i in range(len(list_to_clean) - 1):
         for j in range(i + 1, len(list_to_clean)):
             if list_to_clean[i] == list_to_clean[j] and isinstance(list_to_clean[i], type(list_to_clean[j])):
                 list_to_clean[j] = None
-    list_new = []
-    for i in list_to_clean:
-        if i != None:
-            list_new.append(i)
+    list_new = [i for i in list_to_clean if i != None]
     return list_new
-l = ['qwest',222,222.002,'qwest',1,'qwest',222,'qwest',222,222.0,'222','qwes']
-print(clean_list(l))
