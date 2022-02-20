a2cd202e7d091eed80b3f9110f14d3f6465e73d4
serhiintus <sergius.sport@gmail.com>
2022-02-20 09:48:22 +0100

    rename variables and add comments

--- control_project_1.py
+++ control_project_1.py
@@ -1,22 +1,27 @@
+#program decodes the message encoded with the Beacon's cipher
 import sys
 
-coded_text = sys.argv[1]
+false_message = sys.argv[1]
 key = 'aaaaabbbbbabbbaabbababbaaababaab'
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-decoded_text = ''
-temp = ''
-temp_list = []
+original_message = ''
+ab_group = ''
+groups = []
 
-for i in coded_text:
-    if i != ' ':
-        if i.islower():
-            temp += 'a'
+for i in false_message: 
+    #remove spaces
+    if i != ' ': 
+        #replace characters with "a" and "b"
+        if i.islower(): 
+            ab_group += 'a'
         else:
-            temp += 'b'
-    if len(temp) == 5:
-        temp_list.append(temp)
-        temp = ''
-for i in temp_list:
+            ab_group += 'b'
+    #divide message into groups of 5 characters        
+    if len(ab_group) == 5: 
+        groups.append(ab_group)
+        ab_group = ''
+#replace ab-groups with alphabetic characters
+for i in groups:
     if i in key:
-        decoded_text += alphabet[key.find(i)]
-print(decoded_text)        
+        original_message += alphabet[key.find(i)] 
+print(original_message)        
